1d963dc533d198528b39a25a55030464b085699c
Your Name <you@example.com>
2025-11-06 08:08:41 +0000

    add the libarraies and definition need for simple shell

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#define BUFFER_SIZE 1024
+#define TOKEN_DELIM "\t\r\n\a"
+#define PROMPT "$"
+#endif
