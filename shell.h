6e91faa2f2b84695cdb1c458d1c0085b6224bc1b
Your Name <you@example.com>
2025-11-06 10:00:01 +0000

    add prompt_text define

--- shell.h
+++ shell.h
@@ -1,20 +1,20 @@
 #ifndef SHELL_H
 #define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <limits.h>
-#include <fcntl.h>
 #include <errno.h>
-#define BUFFER_SIZE 1024
-#define TOKEN_DELIM "\t\r\n\a"
-#define PROMPT "$"
 
-void run_noninteractive(char **envp, const char *prog, int *last_code);
+#define PROMPT_TEXT "#cisfun$ "
+
+extern char **environ;
+
 void run_interactive(char **envp, const char *prog, int *last_code);
-int execute_simple(char *cmd, char **envp, const char *prog);
+void run_noninteractive(char **envp, const char *prog, int *last_code);
+int  execute_simple(char *cmd, char **envp, const char *prog);
 
 #endif
