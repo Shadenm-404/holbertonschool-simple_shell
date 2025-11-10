043301e952e6d2afe916a7c68a515228e72641c9
sondos04 <sondos.alrubaih04@gmail.com>
2025-11-10 11:18:46 +0000

    add dispatch and exxit function

--- builtins.c
+++ builtins.c
@@ -23,3 +23,47 @@ int builtin_env(char **args, char **envp)
     }
     return (0);
 }
+
+int bi_exit(char **args, shell_state_t *st)
+{
+	int status = st->exit_status;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "exit\n", 5);
+
+	if (args[1])
+	{
+		if (_isdigit_str(args[1]) && _atoi_posmod(args[1], &status) == 0)
+		{
+			st->should_exit = 1;
+			st->exit_status = status;
+			return (status);
+		}
+		print_exit_illegal(st, args[1]);
+		return (2);
+	}
+	st->should_exit = 1;
+	st->exit_status = status;
+	return (status);
+}
+
+int builtin_dispatch(char **args, shell_state_t *st)
+{
+	int i;
+	builtin_t table[] = {
+		{"env", bi_env},
+		{"exit", bi_exit},
+		{NULL, NULL}
+	};
+
+	for (i = 0; table[i].name; i++)
+	{
+		if (_strcmp(args[0], table[i].name) == 0)
+		{
+			st->exit_status = table[i].func(args, st);
+			return (1);
+		}
+	}
+	return (0);
+}
+
