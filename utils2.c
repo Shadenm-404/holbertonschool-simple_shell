c98e362aee1f9d168b1b4b95701e2bbcc69710e1
sondos04 <sondos.alrubaih04@gmail.com>
2025-11-10 11:59:09 +0000

    creat a atoi function

--- utils2.c
+++ utils2.c
@@ -1,66 +1,31 @@
-
 #include "shell.h"
 
-size_t _strlen(const char *s)
+/**
+ * _atoi - convert a string to an integer
+ * @str: pointer to the string to convert
+ *
+ * Return: integer value represented by str
+ */
+int _atoi(char *str)
 {
-	size_t n = 0;
-	if (!s)
-		return 0;
-	while (s[n])
-		n++;
-	return n;
-}
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-int _strcmp(const char *s1, const char *s2)
-{
-	while (*s1 && *s2 && *s1 == *s2)
+	if (str[0] == '-')
 	{
-		s1++;
-		s2++;
+		sign = -1;
+		i++;
 	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
-}
 
-char *_strdup(const char *s)
-{
-	size_t n, i; char *p;
-	if (!s)
-		return NULL;
-	n = _strlen(s);
-	p = malloc(n + 1);
-	if (!p)
-		return NULL;
-	for (i = 0; i < n; i++)
-		p[i] = s[i];
-	p[n] = '\0';
-	return p;
-}
+	for (; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			break;
+		}
+		result = result * 10 + (str[i] - '0');
+	}
 
-int _isdigit_str(const char *s)
-{
-	size_t i = 0;
-	if (!s || !*s)
-		return 0;
-	if (s[0] == '+')
-		i = 1;
-	for (; s[i]; i++)
-		if (s[i] < '0' || s[i] > '9')
-			return 0;
-	return 1;
-}
-
-int _atoi_posmod(const char *s, int *out)
-{
-	unsigned long v = 0; size_t i = 0;
-	if (!s || !out) return 1;
-	if (s[0] == '+') i = 1;
-	for (; s[i]; i++) v = v * 10 + (unsigned long)(s[i] - '0');
-	*out = (int)(v & 0xFF);
-	return 0;
-}
-
-void print_exit_illegal(shell_state_t *st, const char *arg)
-{
-	dprintf(STDERR_FILENO, "%s: %lu: exit: Illegal number: %s\n",
-		st->prog_name, st->line_count, arg);
+	return (sign * result);
 }
