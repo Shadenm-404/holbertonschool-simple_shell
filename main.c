63ada3d82c6f3b35648b95fdcde83388357496b7
sondos04 <sondos.alrubaih04@gmail.com>
2025-11-10 11:36:33 +0000

    rewrite the file to make only 5 or less in it

--- main.c
+++ main.c
@@ -2,60 +2,6 @@
 
 #define MAX_ARGS 128
 
-static char *fetch_input(void)
-{
-	char *buf = NULL;
-	size_t cap = 0;
-	ssize_t nread;
-
-	nread = getline(&buf, &cap, stdin);
-	if (nread == -1)
-	{
-		free(buf);
-		return (NULL);
-	}
-	if (nread > 0 && buf[nread - 1] == '\n')
-		buf[nread - 1] = '\0';
-	return (buf);
-}
-
-static void trim_ws(char *s)
-{
-	char *start, *end;
-	size_t len;
-
-	if (s == NULL)
-		return;
-
-	start = s;
-	while (*start == ' ' || *start == '\t')
-		start++;
-
-	if (*start == '\0')
-	{
-		s[0] = '\0';
-		return;
-	}
-
-	len = strlen(start);
-	end = start + (len - 1);
-	while (end > start && (*end == ' ' || *end == '\t'))
-		end--;
-	*(end + 1) = '\0';
-
-	if (start != s)
-		memmove(s, start, strlen(start) + 1);
-}
-
-static int is_empty_line(const char *txt)
-{
-	if (txt == NULL)
-		return (1);
-	while (*txt == ' ' || *txt == '\t')
-		txt++;
-	return (*txt == '\0');
-}
-
 void run_interactive(char **envp, const char *prog, int *last_code)
 {
 	char *line;
