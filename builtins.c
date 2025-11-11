2a64dd1b9117a8e703de0b9c7130ee547dc83fe3
sondos04 <sondos.alrubaih04@gmail.com>
2025-11-11 08:15:13 +0000

    edit handle_builtin retun values

--- builtins.c
+++ builtins.c
@@ -110,36 +110,36 @@ int builtin_env(char **args, char **envp)
  */
 int handle_builtin(char **args)
 {
-    if (strcmp(args[0], "exit") == 0)
-    {
-        exit(0);
-    }
-    else if (strcmp(args[0], "env") == 0)
-    {
-        print_env();
-        return (1);
-    }
-    else if (strcmp(args[0], "setenv") == 0)
-    {
-        if (!args[1] || !args[2])
-        {
-            fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
-            return (1);
-        }
-        if (_setenv(args[1], args[2], 1) != 0)
-            fprintf(stderr, "setenv: Failed to set variable\n");
-        return (1);
-    }
-    else if (strcmp(args[0], "unsetenv") == 0)
-    {
-        if (!args[1])
-        {
-            fprintf(stderr, "Usage: unsetenv VARIABLE\n");
-            return (1);
-        }
-        if (_unsetenv(args[1]) != 0)
-            fprintf(stderr, "unsetenv: Failed to unset variable\n");
-        return (1);
-    }
-    return (0); /* Not a builtin */
+	if (strcmp(args[0], "exit") == 0)
+	{
+		exit(0);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		print_env();
+		return (1);
+	}
+	else if (strcmp(args[0], "setenv") == 0)
+	{
+		if (!args[1] || !args[2])
+		{
+			fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
+			return (1);
+		}
+		if (_setenv(args[1], args[2], 1) != 0)
+			fprintf(stderr, "setenv: Failed to set variable\n");
+		return (1);
+	}
+	else if (strcmp(args[0], "unsetenv") == 0)
+	{
+		if (!args[1])
+		{
+			fprintf(stderr, "Usage: unsetenv VARIABLE\n");
+			return (1);
+		}
+		if (_unsetenv(args[1]) != 0)
+			fprintf(stderr, "unsetenv: Failed to unset variable\n");
+		return (1);
+	}
+	return (0); /* Not a builtin */
 }
