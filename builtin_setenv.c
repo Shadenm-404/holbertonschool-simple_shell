76c70e9143bb1ae13f735fd6f87fbb2ba7e81524
sondos04 <sondos.alrubaih04@gmail.com>
2025-11-11 08:11:17 +0000

    creat a function that  modify an environment variable

--- builtin_setenv.c
+++ builtin_setenv.c
@@ -2,45 +2,22 @@
 
 /**
  * _setenv - Initialize or modify an environment variable
- * @name: variable name
- * @value: variable value
- * @overwrite: 1 to overwrite existing variable, 0 to keep
+ * @name: variable name (must not contain '=' and not be empty)
+ * @value: variable value (must not be NULL)
+ * @overwrite: if 0, keep existing; if non-zero, overwrite
  * Return: 0 on success, -1 on failure
  */
 int _setenv(const char *name, const char *value, int overwrite)
 {
-    int i;
-    size_t len;
+	if (!name || !*name || strchr(name, '=') || !value)
+		return (-1);
 
-    if (!name || !value)
-        return (-1);
+	if (!overwrite)
+	{
+		char *old = getenv(name);
 
-    len = strlen(name);
-    for (i = 0; environ[i]; i++)
-    {
-        if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
-        {
-            if (!overwrite)
-                return (0);
-            /* Replace existing variable */
-            free(environ[i]);
-            environ[i] = malloc(len + strlen(value) + 2);
-            if (!environ[i])
-                return (-1);
-            sprintf(environ[i], "%s=%s", name, value);
-            return (0);
-        }
-    }
-
-    /* Add new variable */
-    environ = realloc(environ, sizeof(char *) * (i + 2));
-    if (!environ)
-        return (-1);
-    environ[i] = malloc(len + strlen(value) + 2);
-    if (!environ[i])
-        return (-1);
-    sprintf(environ[i], "%s=%s", name, value);
-    environ[i + 1] = NULL;
-
-    return (0);
+		if (old != NULL)
+			return (0);
+	}
+	return (setenv(name, value, 1));
 }
